bea8f39c0356c1c926e9fa4bdd50e377cd5e91c8
Piotr Adamczyk <xeno@xeno.yt>
2020-03-01 10:13:49 +0100

    update

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+import PatronDaiCampaign from "patrondai-contracts/build/contracts/PatronDaiCampaign.json";
+import { ethers } from "ethers";
+import EthereumContext from "../contexts/EthereumContext";
+import { withRouter } from "react-router";
+
+class ProjectCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: null
+    };
+  }
+  async componentDidMount() {
+    const address = await this.props.registry.getCampaign(this.props.index);
+    this.contract = new ethers.Contract(
+      address,
+      PatronDaiCampaign.abi,
+      this.context.provider
+    );
+    const data = await fetch(
+      "https://centralization.sucks.af/api/campaign/" + address
+    ).then(r => r.json());
+    this.setState({ address, data: data.data });
+  }
+  render() {
+    if (!this.state.data) return "";
+    return (
+      <div
+        onClick={() => {
+          this.props.history.push("/campaign/" + this.state.address);
+        }}
+        class="bx--tile"
+        style={{ textAlign: "left", width: "33%", margin: 10 }}
+      >
+        <div style={{ padding: 4 }}>
+          <h3>{this.state.data.title}</h3>
+        </div>
+        <div style={{ padding: 4 }}>
+          <h4>{this.state.data.description}</h4>
+        </div>
+        <div className="ProjectCard-image" style={{ padding: 4 }}>
+          <img
+            style={{ width: 300, height: 200, objectFit: "cover" }}
+            src={
+              this.state.data.url ? this.state.data.url : this.state.data.image
+            }
+          />
+        </div>
+        <div style={{ opacity: 0.5, padding: 4 }}>{this.state.address}</div>
+      </div>
+    );
+  }
+}
+
+ProjectCard.contextType = EthereumContext;
+
+export default withRouter(ProjectCard);
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,42 @@ import React, { Component } from "react";
 import EthereumContext from "../contexts/EthereumContext";
 import { ethers } from "ethers";
 
+import RinkebyEnv from "patrondai-contracts/.openzeppelin/rinkeby.json";
+import PatronDaiCampaignsRegistry from "patrondai-contracts/build/contracts/PatronDaiCampaignsRegistry.json";
+import ProjectCard from "./ProjectCard";
+
 export default class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campaignsCount: 0
+    };
+  }
   async componentDidMount() {
-    this.contract = new ethers.Contract();
+    this.contract = new ethers.Contract(
+      RinkebyEnv.proxies[
+        "squirvels-contracts/PatronDaiCampaignsRegistry"
+      ][0].address,
+      PatronDaiCampaignsRegistry.abi,
+      this.context.provider
+    );
+    this.setState({
+      campaignsCount: (await this.contract.getCampaignsCount()).toNumber()
+    });
   }
   render() {
-    return <div>test</div>;
+    const projectIndexes = [
+      ...new Array(this.state.campaignsCount).keys()
+    ].reverse();
+    return (
+      <div
+        style={{ padding: "0 22px", display: "flex", justifyContent: "center" }}
+      >
+        {projectIndexes.map(e => (
+          <ProjectCard key={e} index={e} registry={this.contract} />
+        ))}
+      </div>
+    );
   }
 }
 
